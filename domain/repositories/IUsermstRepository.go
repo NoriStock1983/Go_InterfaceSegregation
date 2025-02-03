2f5c75ff053c6d7afbf06204e489af1d9afb14ce
AQUA651 <86642639+NoriStock1983@users.noreply.github.com>
2025-02-03 21:33:50 +0900

    コメント追加

--- domain/repositories/IUsermstRepository.go
+++ domain/repositories/IUsermstRepository.go
@@ -1,6 +1,8 @@
 package repositories
 
 // 検索用のインターフェース
+// 検索処理を行う場合にのみ必要な関数を定義する。
+// 検索処理を行うためだけのインターフェースのため、登録、更新、削除処理は含まない。
 type ISearchUsermstRepository interface {
 	AllSearchUsermst() string
 	CountByUsercd(usercd string) int
@@ -8,12 +10,17 @@ type ISearchUsermstRepository interface {
 }
 
 // Insert用のインターフェース
+// Insert処理を行う場合にのみ必要な関数を定義する。
+// Insertを行う前に、対象データが存在するかどうか確認するためにCountByUsercd関数を定義する。
 type IInsertUsermstRepository interface {
 	CountByUsercd(usercd string) int
 	InsertUsermst() bool
 }
 
 // Update用のインターフェース
+// Update処理を行う場合にのみ必要な関数を定義する。
+// Updateを行う前に、対象データが存在するかどうか確認するためにCountByUsercd関数を定義する。
+// Update処理を実行する前に、対象データが他のユーザに更新されていないかなどを確認するために、SearchByUsercd関数を定義する。
 type IUpdateUsermstRepository interface {
 	CountByUsercd(usercd string) int
 	SearchByUsercd() string
@@ -21,6 +28,9 @@ type IUpdateUsermstRepository interface {
 }
 
 // Delete用のインターフェース
+// Delete処理を行う場合にのみ必要な関数を定義する。
+// Deleteを行う前に、対象データが存在するかどうか確認するためにCountByUsercd関数を定義する。
+// Delete処理を実行する前に、対象データが他のユーザに更新されていないかなどを確認するために、SearchByUsercd関数を定義する。
 type IDeleteUsermstRepository interface {
 	CountByUsercd(usercd string) int
 	SearchByUsercd() string
